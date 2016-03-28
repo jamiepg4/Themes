ae0f5acde6989a94e27241cd0d6a5782401909fe
Frederick MC Chan <meangczac.chan@gmail.com>
2016-03-29 07:03:37 +0800

    Setup Minimal Theme

---
+++ themes/minimal/minimal.php
@@ -0,0 +1,37 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) 2016 PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Name: Minimal Theme
+| Filename: minimal.php
+| Version: 1.00
+| Author: Blacktie.co, PHP-Fusion Inc.
+| Site: http://www.blacktie.co
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+
+
+class MinimalTheme {
+
+    public function __construct() {
+
+    }
+
+    public function render_page() {
+
+    }
+
+
+
+
+
+}
